acaa6cd9701484595b989ea5d3125e9c78a278ec
Juan Jose Sapuyes <jsapuyesp@gmail.com>
2022-06-24 21:52:12 -0500

    Changes

--- preguntas.py
+++ preguntas.py
@@ -114,7 +114,7 @@ def pregunta_03():
     # Defina un diccionario de parámetros para el GridSearchCV. Se deben
     # considerar valores desde 1 hasta 11 regresores para el modelo
     param_grid = {
-        "regression_model_fit_intercept": list(range(1, 12)),
+        "regression_model__fit_intercept": list(range(1,12)),
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
